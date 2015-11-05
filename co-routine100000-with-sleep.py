3b1e13ff42c67c5c7061c3ed29da4e6e96b3e38f
yuzuru.nakamura <yuzuru.nakamura@medpeer.co.jp>
2015-11-05 12:41:30 +0900

    add test-script

---
+++ co-routine100000-with-sleep.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import asyncio
+
+
+def fizzbuzz(i):
+    if i == 15:
+        return 'FizzBuzz'
+    if i % 5 == 0:
+        return 'Buzz'
+    if i % 3 == 0:
+        return 'Fizz'
+
+    return str(i)
+
+
+async def task_fizzbuzz(prefix):
+    for x in range(1, 10):
+        await asyncio.sleep(1)
+        print(prefix + '{}:'.format(str(x)) + fizzbuzz(x))
+    return None
+
+loop = asyncio.get_event_loop()
+
+tasks = asyncio.wait([task_fizzbuzz(str(i) + ':') for i in range(1, 100000)])
+loop.run_until_complete(tasks)
+loop.close()
